abd4faf9a712d8df0a8982d78583dca50c2668c0
iaalvearp <iaalvearp@gmail.com>
2025-04-05 00:54:01 -0500

    Adding reusable templates and new assets

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+import animated from '@midudev/tailwind-animations'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        fontFamily: {
+            'athelas': 'athelas',
+            'avenir': 'avenir',
+            'sweet': 'sweet'
+        },
+        colors: {
+            'primary': '#3D4439',
+            'text': '#EEE2D2',
+            'accent': '#B45E30',
+            'bg': '#F9F3E9',
+            'pink': '#F4DEBE'
+        }
+    },
+    plugins: [animated],
+}
